ea248b9216420c7dd8528f1336f15532b2fb4939
Grigorii Sharapov <socaseinpoint@gmail.com>
2020-12-13 19:53:06 +0600

    add reexport for components and services

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Button } from "./Button";
+export { default as Form } from "./Form";
+export { default as Field } from "./Field";
+export { default as Input } from "./Input";
+export { default as PasswordInput } from "./PasswordInput";
---
+++ src/services/index.ts
@@ -0,0 +1,1 @@
+export { default as isRequired } from "./isRequired";
